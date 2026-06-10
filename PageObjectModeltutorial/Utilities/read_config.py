fdc9f3ac1071f7d23a4ad065895fb177ce9e7410
Jananisri14 <jananisathishkumar380@gmail.com>
2026-06-10 20:41:37 +0530

    Update read_config.py

--- PageObjectModeltutorial/Utilities/read_config.py
+++ PageObjectModeltutorial/Utilities/read_config.py
@@ -1,20 +1,5 @@
 from configparser import ConfigParser
-import os
-
-def get_config(category, key):
-    config = ConfigParser()
-
-    current_dir = os.path.dirname(os.path.abspath(__file__))
-    config_path = os.path.join(
-        os.path.dirname(current_dir),
-        "Configuration",
-        "config.ini"
-    )
-
-    print("Config Path:", config_path)
-
-    config.read(config_path)
-
-    print("Sections:", config.sections())
-
+def get_config(category,key):
+    config=ConfigParser()
+    config.read("./Configurations/config.ini")
     return config.get(category, key)
